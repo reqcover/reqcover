aa3796ba628d7c276d707c80fa11b66744db8ef0
Christian Hujer <Christian.Hujer@nelkinda.com>
2025-09-15 20:13:30 +0530

    refactor: Simplify DOM code with extension methods.

--- reqcover-reporter-html/src/jvmMain/kotlin/dev/reqcover/reporter/html/HtmlRequirementsCoverageReporter.kt
+++ reqcover-reporter-html/src/jvmMain/kotlin/dev/reqcover/reporter/html/HtmlRequirementsCoverageReporter.kt
@@ -2,6 +2,8 @@ package dev.reqcover.reporter.html
 
 import dev.reqcover.engine.RequirementsCoverageTracker
 import dev.reqcover.engine.spi.RequirementsCoverageReporter
+import org.w3c.dom.Element
+import org.w3c.dom.Text
 import org.w3c.dom.bootstrap.DOMImplementationRegistry
 import org.w3c.dom.ls.DOMImplementationLS
 import java.io.OutputStream
@@ -9,6 +11,10 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 class HtmlRequirementsCoverageReporter : RequirementsCoverageReporter {
+    companion object {
+        const val XMLNS_XHTML = "http://www.w3.org/1999/xhtml"
+    }
+
     override fun report(tracker: RequirementsCoverageTracker) {
         val path = Path.of(System.getProperty("user.dir"), "build", "reports", "tests", "reqcover", "requirements-coverage.html")
         val parentDir = path.parent
@@ -25,72 +31,60 @@ class HtmlRequirementsCoverageReporter : RequirementsCoverageReporter {
     ): Boolean {
         val domImplementationLS = DOMImplementationRegistry.newInstance().getDOMImplementation("LS")
         if (domImplementationLS !is DOMImplementationLS) throw IllegalStateException("Could not get DOMImplementationLS")
-        val document = domImplementationLS.createDocument("http://www.w3.org/1999/xhtml", "html", domImplementationLS.createDocumentType("html", null, null))
+        val document = domImplementationLS.createDocument(XMLNS_XHTML, "html", domImplementationLS.createDocumentType("html", null, null))
         val rootElement = document.documentElement
-        val headElement = document.createElementNS("http://www.w3.org/1999/xhtml", "head")
-        rootElement.appendChild(headElement)
-        val titleElement = document.createElementNS("http://www.w3.org/1999/xhtml", "title")
-        headElement.appendChild(titleElement)
-        val titleText = document.createTextNode("Requirements Coverage Report")
-        titleElement.appendChild(titleText)
-        val bodyElement = document.createElementNS("http://www.w3.org/1999/xhtml", "body")
-        rootElement.appendChild(bodyElement)
-        val h1Element = document.createElementNS("http://www.w3.org/1999/xhtml", "h1")
-        bodyElement.appendChild(h1Element)
-        val h1Text = document.createTextNode("Requirements Coverage Report")
-        h1Element.appendChild(h1Text)
+        val headElement = rootElement.appendElement("head")
+        headElement.appendElementWithText("title", "Requirements Coverage Report")
+        val bodyElement = rootElement.appendElement("body")
+        bodyElement.appendElementWithText("h1", "Requirements Coverage Report")
 
-        val tableElement = document.createElementNS("http://www.w3.org/1999/xhtml", "table")
-        bodyElement.appendChild(tableElement)
-        val theadElement = document.createElementNS("http://www.w3.org/1999/xhtml", "thead")
-        tableElement.appendChild(theadElement)
-        val headerRowElement = document.createElementNS("http://www.w3.org/1999/xhtml", "tr")
-        theadElement.appendChild(headerRowElement)
-        val thRequirementId = document.createElementNS("http://www.w3.org/1999/xhtml", "th")
-        headerRowElement.appendChild(thRequirementId)
-        val thRequirementIdText = document.createTextNode("Requirement ID")
-        thRequirementId.appendChild(thRequirementIdText)
-        val thTest = document.createElementNS("http://www.w3.org/1999/xhtml", "th")
-        headerRowElement.appendChild(thTest)
-        val thTestText = document.createTextNode("Test")
-        thTest.appendChild(thTestText)
+        val tableElement = bodyElement.appendElement("table")
+        val theadElement = tableElement.appendElement("thead")
+        val headerRowElement = theadElement.appendElement("tr")
+        headerRowElement.appendElementWithText("th", "Requirement ID")
+        headerRowElement.appendElementWithText("th", "Test")
 
-        val tbodyElement = document.createElementNS("http://www.w3.org/1999/xhtml", "tbody")
-        tableElement.appendChild(tbodyElement)
+        val tbodyElement = tableElement.appendElement("tbody")
         for (requirementId in tracker.expectedRequirements()) {
-            val rowElement = document.createElementNS("http://www.w3.org/1999/xhtml", "tr")
-            tbodyElement.appendChild(rowElement)
-            val tdRequirementId = document.createElementNS("http://www.w3.org/1999/xhtml", "td")
-            rowElement.appendChild(tdRequirementId)
-            val tdRequirementIdText = document.createTextNode(requirementId)
-            tdRequirementId.appendChild(tdRequirementIdText)
-            val tdTest = document.createElementNS("http://www.w3.org/1999/xhtml", "td")
-            rowElement.appendChild(tdTest)
+            val rowElement = tbodyElement.appendElement("tr")
+            rowElement.appendElementWithText("td", requirementId)
+            val tdTest = rowElement.appendElement("td")
             val testText = if (tracker.verifiedRequirements().contains(requirementId)) {
                 "Covered"
             } else {
                 "- (Not Covered)"
             }
-            val tdTestText = document.createTextNode(testText)
-            tdTest.appendChild(tdTestText)
+            tdTest.appendText(testText)
         }
 
         for (requirementId in tracker.unexpectedRequirements()) {
-            val rowElement = document.createElementNS("http://www.w3.org/1999/xhtml", "tr")
-            tbodyElement.appendChild(rowElement)
-            val tdRequirementId = document.createElementNS("http://www.w3.org/1999/xhtml", "td")
-            rowElement.appendChild(tdRequirementId)
-            val tdRequirementIdText = document.createTextNode(requirementId)
-            tdRequirementId.appendChild(tdRequirementIdText)
-            val tdTest = document.createElementNS("http://www.w3.org/1999/xhtml", "td")
-            rowElement.appendChild(tdTest)
-            val tdTestText = document.createTextNode("Unexpected")
-            tdTest.appendChild(tdTestText)
+            val rowElement = tbodyElement.appendElement("tr")
+            rowElement.appendElementWithText("td", requirementId)
+            rowElement.appendElementWithText("td", "Unexpected")
         }
 
         val serializer = domImplementationLS.createLSSerializer()
         val output = domImplementationLS.createLSOutput()
         output.byteStream = outputStream
         return serializer.write(document, output)
+    }
+
+    fun Element.appendElement(tagName: String): Element {
+        val newChild = ownerDocument.createElementNS(XMLNS_XHTML, tagName)
+        appendChild(newChild)
+        return newChild
+    }
+
+    fun Element.appendText(text: String): Text {
+        val textNode = ownerDocument.createTextNode(text)
+        appendChild(textNode)
+        return textNode
+    }
+
+    fun Element.appendElementWithText(tagName: String, text: String): Element {
+        val newChild = ownerDocument.createElementNS(XMLNS_XHTML, tagName)
+        newChild.appendChild(ownerDocument.createTextNode(text))
+        appendChild(newChild)
+        return newChild
     }
 }
